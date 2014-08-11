a72b5a4a2fa64e066b1a955a8dce37827d125c49
Thomas Babu <tommy.developer@gmail.com>
2014-08-12 00:16:39 +0800

    updated bootstrap 3

--- app/views/layouts/users.blade.php
+++ app/views/layouts/users.blade.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 		<style>
 			table form { margin-bottom: 0; }
 			form ul { margin-left: 0; list-style: none; }
@@ -12,6 +12,21 @@
 	</head>
 	<body>
 		<div class="container">
+
+			<nav class="navbar navbar-inverse">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="{{ URL::to('users') }}">My Users</a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="{{ URL::to('users')}}">View All Users</a>
+					</li>
+					<li>
+						<a href="{{ URL::to('users/create') }}">Create a User</a>
+					</li>
+				</ul>
+			</nav>
+
 			@if (Session::has('message'))
 				<div class="flash alert">
 					<p> {{ Session::get('message')}}
--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -10,12 +10,13 @@
     <table class="table table-striped table-bordered">
         <thead>
             <tr>
+                <th>Name</th>
                 <th>Username</th>
                 <th>Password</th>
                 <th>Email</th>
-                <th>Phone</th>
-                <th>Name</th>
-                <th>Actions</th>
+                <th>Phone</th>                
+                <th></th>
+                <th></th>
             </tr>
         </thead>
 
@@ -23,11 +24,11 @@
             @foreach ($users as $user)
 
             <tr>
+                <td>{{$user->name}}</td>
                 <td>{{$user->username}}</td>
                 <td>{{$user->password}}</td>
                 <td>{{$user->email}}</td>
-                <td>{{$user->phone}}</td>
-                <td>{{$user->name}}</td>
+                <td>{{$user->phone}}</td>                
                 <td>{{ link_to_route('users.edit', 'Edit', array($user->id), array('class'=> 'btn btn-info')) }}</td>
                 <td>
                     {{ Form::open(array('method'=> 'DELETE', 'route' => array('users.destroy', $user->id)))}} 
